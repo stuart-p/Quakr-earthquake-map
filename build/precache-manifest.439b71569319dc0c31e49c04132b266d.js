6d2983085519e2f1a6cce1828751414be98794ec
stuart-p <stuart_palmer85@hotmail.com>
2020-02-19 22:33:11 +0000

    prep for deployment

---
+++ build/precache-manifest.439b71569319dc0c31e49c04132b266d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c057fdd6803f5d27b41c757e8d3b2498",
+    "url": "/quakr/index.html"
+  },
+  {
+    "revision": "943fff2d81bac4d3e7b3",
+    "url": "/quakr/static/css/main.0d2e0640.chunk.css"
+  },
+  {
+    "revision": "084703ffe4b388461ff4",
+    "url": "/quakr/static/js/2.36d96642.chunk.js"
+  },
+  {
+    "revision": "14576e628ce8fe44573c43763ba1508d",
+    "url": "/quakr/static/js/2.36d96642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "943fff2d81bac4d3e7b3",
+    "url": "/quakr/static/js/main.0d8546ca.chunk.js"
+  },
+  {
+    "revision": "fee149e45d88edefc85b",
+    "url": "/quakr/static/js/runtime-main.198acfae.js"
+  }
+]);
